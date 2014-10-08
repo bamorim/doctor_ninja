6a96d0eb9a813a68635dcf26c97dbd3179980660
Bernardo Amorim <contato@bamorim.com>
2014-10-08 15:02:23 -0300

    Debug mode on parser.rb

--- lib/ninjadoc/parser.rb
+++ lib/ninjadoc/parser.rb
@@ -13,12 +13,38 @@ module Ninjadoc
     end
 
     def parse_node(node)
+      parsers = parsers_for(node)
+
+      if debug?(node,parsers)
+        debug(node,binding)
+      end
+
+      parsers
+        .first
+        .parse
+    end
+
+    def parsers_for(node)
       Ninjadoc::Parsers
         .parsers
         .select{|p| p.applicable_to? node}
         .map{|p| p.new(node, self.public_method(:parse_node))}
-        .first
-        .parse
+    end
+
+    def debug?(node,parsers)
+      ENV["DEBUG"] == "all" || 
+      (ENV["DEBUG"] == "missing" && parsers.length == 1) || 
+      ENV["DEBUG"] == node.name ||
+      ENV["DEBUG"] == "#{node.namespace.prefix}:#{node.name}"
+    end
+
+    def debug(node,b)
+      if(ENV["DEBUG_MODE"]=="pry")
+        require "pry"
+        b.pry
+      else
+        puts "---BEGIN---\n#{node.to_xml}\n----END----"
+      end
     end
   end
 end
